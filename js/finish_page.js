6b5b9d37dfb2fe0c7588d2d40fe6a068f68ea762
Daniella Alamo <dalamov08@gmail.com>
2021-07-08 22:41:28 +0200

    Adding checkbox validation

--- js/finish_page.js
+++ js/finish_page.js
@@ -6,19 +6,16 @@ let $finishDivToRemove = document.querySelector(".finish-div-to-remove");
 let $termsWarning = document.getElementById("terms-warning");
 let $termsCheckBox = document.getElementById("cbox2");
 
-/*
-if ($termsCheckBox.checked == false) {
-    $termsWarning.innerHTML = "Please accept the terms and conditions";
-    $termsWarning.classList.add("input-warning");
-}
-$finishBuyBtn.addEventListener("click", (event) => {
-    $finishDivToRemove.innerHTML = "Gracias por su compra" + " &#9829;";
-});*/
 
-function checkCheckBox(f) {
-    if (f.agree.checked == false) {
+
+$finishBuyBtn.addEventListener("click", (event) => {
+    if ($termsCheckBox.checked) {
+        $finishDivToRemove.innerHTML = "Gracias por su compra" + " &#9829;";
+    }
+    else  {
         $termsWarning.innerHTML = "Please accept the terms and conditions";
-        return false;
-    } else
-        return true;
-}
+        $termsWarning.classList.add("input-warning");
+    } 
+    
+});
+
